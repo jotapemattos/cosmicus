a961984c3e39819321c65210a9fdfcbd9049e05d
Lucas P1nheiro <lps1704@gmail.com>
2024-04-11 12:59:55 -0300

    chore: setup eslint and prettier

--- components/google-auth-button.tsx
+++ components/google-auth-button.tsx
@@ -1,28 +1,28 @@
 'use client'
-import { createClient } from "@/utils/supabase/client";
-import Github from "./icons/github"
-import Google from "./icons/google";
-
+import { createClient } from '@/utils/supabase/client'
+import Google from './icons/google'
 
 const GoogleAuthButton = () => {
-
   const supabase = createClient()
-  
+
   const handleLoginWithGoogle = () => {
     supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${location.origin}/auth/callback`
-      }
+        redirectTo: `${location.origin}/auth/callback`,
+      },
     })
   }
 
   return (
-      <button className="flex items-center gap-2 justify-center w-full mb-4 border-gray-800 border py-2 rounded-md hover:bg-white/10 transition-all duration-300" onClick={handleLoginWithGoogle}>
-          <Google className="w-4 h-4"/>
-          <span>Entrar com Google</span>
-      </button>
+    <button
+      className="flex items-center gap-2 justify-center w-full mb-4 border-gray-800 border py-2 rounded-md hover:bg-white/10 transition-all duration-300"
+      onClick={handleLoginWithGoogle}
+    >
+      <Google className="w-4 h-4" />
+      <span>Entrar com Google</span>
+    </button>
   )
 }
 
-export default GoogleAuthButton 
+export default GoogleAuthButton
