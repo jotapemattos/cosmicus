bcd8f4b720810582398d119cb1afcd91da15a112
João Pedro <jpmrc49@gmail.com>
2024-04-26 15:15:53 -0300

    feat(header): adds react-query

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,25 @@
-import AuthButton from './auth-button'
+import { createClient } from '@/utils/supabase/server'
+import UserNav from './user-nav'
+import Link from 'next/link'
 
-export default function Header() {
+export default async function Header() {
+  const supabase = createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
   return (
     <header className="absolute top-0 flex w-full items-center justify-center border-b bg-background py-4">
-      <AuthButton />
+      {user ? (
+        <UserNav user={user} />
+      ) : (
+        <Link
+          href="/sign-in"
+          className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
+        >
+          Entrar
+        </Link>
+      )}
     </header>
   )
 }
---
+++ components/user-nav.tsx
@@ -0,0 +1,52 @@
+'use client'
+import Link from 'next/link'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { User } from '@supabase/supabase-js'
+import { useQuery } from '@tanstack/react-query'
+import { getProfileByUserId } from '@/data/profile'
+import { signOut } from '@/app/actions/sign-out'
+
+interface UserNavProps {
+  user: User
+}
+export default function UserNav({ user }: UserNavProps) {
+  const { data: profile } = useQuery({
+    queryKey: ['profile', user.id],
+    queryFn: () => getProfileByUserId({ userId: user.id }),
+  })
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={profile?.picture ?? undefined} />
+          <AvatarFallback>
+            {profile?.username?.substring(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem asChild className="cursor-pointer">
+          <Link href={`/profile/${user.id}`}> Meu Perfil</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="cursor-pointer">
+          <form action={signOut} className="w-full">
+            <button className="w-full text-start" type="submit">
+              Sair
+            </button>
+          </form>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
