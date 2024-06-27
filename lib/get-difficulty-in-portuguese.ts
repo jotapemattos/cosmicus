7d7abc610fd87129724b5d600d7c448809ae06eb
Lucas P1nheiro <lps1704@gmail.com>
2024-06-27 12:27:28 -0300

    refactor: use Map to get difficulty in portuguese

--- lib/get-difficulty-in-portuguese.ts
+++ lib/get-difficulty-in-portuguese.ts
@@ -2,15 +2,14 @@ interface GetDifficultyInPortugueseProps {
   difficulty: 'easy' | 'medium' | 'hard'
 }
 
+const difficultyMap = new Map<string, string>([
+  ['easy', 'fácil'],
+  ['medium', 'médio'],
+  ['hard', 'difícil'],
+])
+
 export function getDifficultyInPortuguese({
   difficulty,
 }: GetDifficultyInPortugueseProps) {
-  switch (difficulty) {
-    case 'easy':
-      return 'fácil'
-    case 'medium':
-      return 'médio'
-    case 'hard':
-      return 'difícil'
-  }
+  return difficultyMap.get(difficulty)
 }
