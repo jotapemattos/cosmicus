cb3169bf4fde70e23a3418888e86137834154b2e
João Pedro <jpmrc49@gmail.com>
2024-08-02 18:06:57 -0300

    refactor(leaderboard): replaces table with tanstack table

--- app/leaderboard/leaderboard.tsx
+++ app/leaderboard/leaderboard.tsx
@@ -1,17 +1,5 @@
 'use client'
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from '@/components/ui/table'
-import { getProfiles } from '@/app/actions/profile'
-import { useQuery } from '@tanstack/react-query'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import {
   DropdownMenu,
@@ -22,97 +10,30 @@ import {
   DropdownMenuCheckboxItem,
 } from '@/components/ui/dropdown-menu'
 import { ArrowDownWideNarrow, Search, X } from 'lucide-react'
-import { useEffect, useState } from 'react'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from '@/components/ui/pagination'
-import { cn } from '@/lib/utils'
-
-import { User } from '@supabase/supabase-js'
 import { Input } from '@/components/ui/input'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { getUserLevel } from '@/lib/get-user-level'
-import LevelBadge from '@/components/level-badge'
-
-interface LeaderboardProps {
-  user: User
-}
+import { User } from '@supabase/supabase-js'
+import useGetProfilePosition from '@/hooks/use-get-profile-position'
+import { DataTable } from './data-table'
+import { useState } from 'react'
 
 interface Search {
   search: string
 }
+interface LeaderboardProps {
+  user: User
+}
 
 const Leaderboard = ({ user }: LeaderboardProps) => {
-  const searchParams = useSearchParams()
+  // TODO - implementar a funcao des busca
+  const { columns, profiles, ascending, positions, setAscending } =
+    useGetProfilePosition()
 
-  const router = useRouter()
-
-  const pathname = usePathname()
-
-  const [ascending, setAscending] = useState(false)
-  const [positions, setPositions] = useState<Record<string, number>>({})
-
-  const { register, handleSubmit, reset } = useForm<{ search: string }>()
-
-  const { data: profiles } = useQuery({
-    queryKey: ['profiles', ascending],
-    queryFn: () => getProfiles({ ascending }),
-  })
-
-  // keep tracking of the position of each profile (couldn't use index because of the sorting feature)
-  useEffect(() => {
-    if (profiles) {
-      const sortedProfiles = [...profiles].sort(
-        (a, b) => b.experience_points - a.experience_points,
-      )
-      const updatedPositions = sortedProfiles.reduce(
-        (acc, profile, index) => {
-          acc[profile.id] = index + 1
-          return acc
-        },
-        {} as Record<string, number>,
-      )
-      setPositions(updatedPositions)
-    }
-  }, [profiles, ascending])
+  const [globalFilter, setGlobalFilter] = useState('')
 
   if (profiles === undefined) {
     return
   }
-
-  const handleSearch: SubmitHandler<Search> = (data) => {
-    const searchQuery = data.search
-
-    router.push(`/leaderboard?search=${searchQuery}&page=1`)
-  }
-
-  const handleSearchClear = () => {
-    reset({ search: '' })
-
-    router.push(`/leaderboard`)
-  }
-
-  const search = searchParams.get('search')
-    ? String(searchParams.get('search'))
-    : ''
-
-  const pageRange = 3
-
-  const totalPages = Math.ceil(profiles.length / pageRange)
-
-  const currentPage = Number(searchParams.get('page')) || 1
-
-  const shouldShowFirstPage = currentPage > 9
-
-  const shouldShowLastPage = profiles.length - currentPage > 9
 
   const userPosition = positions[user.id] || null
 
@@ -125,27 +46,27 @@ const Leaderboard = ({ user }: LeaderboardProps) => {
           </p>
         )}
         <div className="flex items-center gap-4">
-          <form
-            className="relative ml-auto flex-1 md:grow-0"
-            onSubmit={handleSubmit(handleSearch)}
-          >
+          <div className="relative ml-auto flex-1 md:grow-0">
             <Search className="absolute left-2.5 top-2 h-4 w-4 text-muted-foreground" />
             <Input
               className="h-8 w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
               placeholder="Buscar jogador"
-              {...register('search')}
+              value={globalFilter}
+              onChange={(e) => setGlobalFilter(e.target.value)}
             />
-            <Button
-              size={'icon'}
-              className="absolute right-2 top-2 size-4 rounded-full"
-              onClick={handleSearchClear}
-              type="button"
-              variant="ghost"
-            >
-              <span className="sr-only">Limpar busca</span>
-              <X />
-            </Button>
-          </form>
+            {globalFilter && (
+              <Button
+                size={'icon'}
+                className="absolute right-2 top-2 size-4 rounded-full"
+                type="button"
+                variant="ghost"
+                onClick={() => setGlobalFilter('')}
+              >
+                <span className="sr-only">Limpar busca</span>
+                <X />
+              </Button>
+            )}
+          </div>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="outline" size="sm" className="h-8 gap-1 text-sm">
@@ -172,144 +93,7 @@ const Leaderboard = ({ user }: LeaderboardProps) => {
           </DropdownMenu>
         </div>
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Posição</TableHead>
-            <TableHead>Nome</TableHead>
-            <TableHead>Pontos de Experiência</TableHead>
-            <TableHead>Level</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {profiles
-            .filter((profile) =>
-              profile.username?.toLowerCase().includes(search?.toLowerCase()),
-            )
-            .slice((currentPage - 1) * pageRange, currentPage * pageRange)
-            .map((profile) => {
-              const { level } = getUserLevel({
-                experiencePoints: profile.experience_points as number,
-              })
-              return (
-                <TableRow key={profile.id}>
-                  <TableCell className="font-medium">
-                    {positions[profile.id]}
-                  </TableCell>
-                  <TableCell className="flex w-1/2 items-center gap-4">
-                    <Avatar className="group size-8">
-                      <AvatarImage src={profile?.picture ?? undefined} />
-                      <AvatarFallback>
-                        {profile?.username?.substring(0, 2).toUpperCase()}
-                      </AvatarFallback>
-                    </Avatar>
-                    {profile.username}
-                  </TableCell>
-                  <TableCell>{profile.experience_points}</TableCell>
-                  <TableCell>
-                    <LevelBadge userLevel={level} />
-                  </TableCell>
-                  <TableCell className="text-right">
-                    <Button asChild variant={'link'}>
-                      <Link href={`/profile/${profile.id}`}>Ver perfil</Link>
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              )
-            })}
-        </TableBody>
-      </Table>
-      <Pagination>
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious
-              href={{
-                pathname,
-                query: { page: currentPage === 1 ? 1 : currentPage - 1 },
-              }}
-              scroll={false}
-            />
-          </PaginationItem>
-          {shouldShowFirstPage && (
-            <>
-              <PaginationItem>
-                <PaginationLink
-                  href={{
-                    pathname,
-                    query: { page: 1 },
-                  }}
-                  scroll={false}
-                >
-                  1
-                </PaginationLink>
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            </>
-          )}
-          {[...Array(profiles)].map((_, index) => {
-            const pageNumber = index + 1
-
-            const pageDifference = Math.abs(pageNumber - currentPage)
-
-            const shouldShowPage = pageDifference < 3
-
-            if (shouldShowPage) {
-              return (
-                <PaginationItem
-                  key={index}
-                  className={cn({
-                    'hidden lg:flex': pageDifference > 0,
-                  })}
-                >
-                  <PaginationLink
-                    href={{
-                      pathname,
-                      query: { page: pageNumber },
-                    }}
-                    isActive={pageNumber === currentPage}
-                    scroll={false}
-                  >
-                    {pageNumber}
-                  </PaginationLink>
-                </PaginationItem>
-              )
-            }
-            return null
-          })}
-          {shouldShowLastPage && (
-            <>
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-              <PaginationItem>
-                <PaginationLink
-                  href={{
-                    pathname,
-                    query: { page: profiles.length },
-                  }}
-                  scroll={false}
-                >
-                  {profiles.length}
-                </PaginationLink>
-              </PaginationItem>
-            </>
-          )}
-          <PaginationItem>
-            <PaginationNext
-              href={{
-                pathname,
-                query: {
-                  page:
-                    currentPage >= totalPages ? totalPages : currentPage + 1,
-                },
-              }}
-              scroll={false}
-            />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
+      <DataTable columns={columns} data={profiles} />
     </section>
   )
 }
---
+++ hooks/use-get-profile-position.tsx
@@ -0,0 +1,108 @@
+import { getProfiles } from '@/app/actions/profile'
+import { useQuery } from '@tanstack/react-query'
+import { useState, useEffect } from 'react'
+import LevelBadge from '@/components/level-badge'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Profile } from '@/db/custom-types'
+import { getUserLevel } from '@/lib/get-user-level'
+import { ColumnDef } from '@tanstack/react-table'
+import Link from 'next/link'
+
+export default function useGetProfilePosition() {
+  const [ascending, setAscending] = useState(false)
+  const [positions, setPositions] = useState<Record<string, number>>({})
+
+  const { data: profiles } = useQuery({
+    queryKey: ['profiles', ascending],
+    queryFn: () => getProfiles({ ascending }),
+  })
+
+  // keep tracking of the position of each profile (couldn't use index because of the sorting feature)
+  useEffect(() => {
+    if (profiles) {
+      const sortedProfiles = [...profiles].sort(
+        (a, b) => b.experience_points - a.experience_points,
+      )
+      const updatedPositions = sortedProfiles.reduce(
+        (acc, profile, index) => {
+          acc[profile.id] = index + 1
+          return acc
+        },
+        {} as Record<string, number>,
+      )
+      setPositions(updatedPositions)
+    }
+  }, [profiles, ascending])
+
+  const columns: ColumnDef<Profile>[] = [
+    {
+      accessorKey: 'id',
+      header: 'Posição',
+      cell: (info) => {
+        const profileId = info.getValue() as string
+
+        return <span className="font-medium">#{positions[profileId]}</span>
+      },
+    },
+    {
+      header: 'Nome',
+      accessorFn: (row) => ({
+        username: row.username,
+        picture: row.picture,
+      }),
+      cell: ({ getValue }) => {
+        const { username, picture } = getValue() as {
+          username: string
+          picture: string
+        }
+        return (
+          <div className="flex items-center gap-2">
+            <Avatar className="group size-8">
+              <AvatarImage src={picture ?? undefined} />
+              <AvatarFallback>
+                {username.substring(0, 2).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <span>{username}</span>
+          </div>
+        )
+      },
+    },
+    {
+      accessorKey: 'experience_points',
+      header: 'Pontos de Experiência',
+      cell: ({ row }) => {
+        const profileXp = parseInt(row.getValue('experience_points'))
+        return <span>{profileXp}</span>
+      },
+    },
+    {
+      accessorKey: 'experience_points',
+      header: 'Level',
+      cell: ({ row }) => {
+        const profileXp = parseInt(row.getValue('experience_points'))
+        const { level } = getUserLevel({
+          experiencePoints: profileXp,
+        })
+
+        return <LevelBadge userLevel={level} />
+      },
+    },
+    {
+      accessorKey: 'id',
+      header: '',
+      cell: ({ row }) => {
+        const userId = parseInt(row.getValue('id'))
+
+        return (
+          <Button asChild variant={'link'}>
+            <Link href={`/profile/${userId}`}>Ver perfil</Link>
+          </Button>
+        )
+      },
+    },
+  ]
+
+  return { columns, profiles, ascending, setAscending, positions }
+}
