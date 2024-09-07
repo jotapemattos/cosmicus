83cd74e3c0c0fc8956c5c0f779669b8661a0f52e
João Pedro <jpmrc49@gmail.com>
2024-09-07 16:23:25 -0300

    feat: shows header only if user exists

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import Header from '@/components/header'
 import { Toaster } from '@/components/ui/sonner'
 import { ReactQueryClientProvider } from '@/components/react-query-client-provider'
+import { createClient } from '@/utils/supabase/server'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -14,16 +15,22 @@ export const metadata = {
   description: 'The fastest way to build apps with Next.js and Supabase',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const supabase = createClient()
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
   return (
     <html lang="pt-BR" className={GeistSans.className}>
       <ReactQueryClientProvider>
         <body className="max-w-screen bg-background text-foreground">
-          <Header />
+          {user && <Header />}
           <main className="flex min-h-screen flex-col items-center">
             {children}
           </main>
