dfdc5edccbb5be0f65825472b3d144b9702b7327
Lucas P1nheiro <lps1704@gmail.com>
2024-09-09 10:12:16 -0300

    style: changes number-ticker text-style

--- components/magic-ui/number-ticker.tsx
+++ components/magic-ui/number-ticker.tsx
@@ -52,7 +52,7 @@ export default function NumberTicker({
   return (
     <span
       className={cn(
-        'inline-block tabular-nums tracking-wider text-black dark:text-white',
+        'inline-block tabular-nums tracking-wider text-foreground',
         className,
       )}
       ref={ref}
