cff58094dd3fdeb81d92b2d6eb7afa51e9ee946d
Lucas P1nheiro <lps1704@gmail.com>
2024-10-31 14:24:14 -0300

    style: updates icons

--- components/command-menu.tsx
+++ components/command-menu.tsx
@@ -14,7 +14,6 @@ import {
   Gamepad2,
   LogOut,
   Medal,
-  Shirt,
   Store,
   Sword,
   User as UserIcon,
@@ -130,7 +129,22 @@ export function CommandMenu({ user }: CommandMenuProps) {
                 </CommandItem>
                 <CommandItem asChild className="hover:cursor-pointer">
                   <Link href="/inventory" onClick={() => setOpen(!open)}>
-                    <Shirt />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="18"
+                      height="18"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="lucide lucide-chest"
+                    >
+                      <path d="M8 19a2 2 0 0 0 2-2V9a4 4 0 0 0-8 0v8a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a4 4 0 0 0-4-4H6" />
+                      <path d="M2 11h20" />
+                      <path d="M16 11v3" />
+                    </svg>
                     Inventário
                   </Link>
                 </CommandItem>
--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -14,6 +14,7 @@ import { User } from '@supabase/supabase-js'
 import { useQuery } from '@tanstack/react-query'
 import { getProfileByUserId } from '@/app/actions/profile'
 import { signOut } from '@/app/actions/sign-out'
+import { LogOut, UserIcon } from 'lucide-react'
 
 interface UserNavProps {
   user: User
@@ -37,14 +38,39 @@ export default function UserNav({ user }: UserNavProps) {
         <DropdownMenuLabel>{user.email}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem asChild className="cursor-pointer">
-          <Link href={`/profile/${user.id}`}>Meu Perfil</Link>
+          <Link href={`/profile/${user.id}`}>
+            <UserIcon size={18} />
+            <span className="ml-2">Meu Perfil</span>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuItem asChild className="cursor-pointer">
-          <Link href="/inventory">Inventário</Link>
+          <Link href="/inventory">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-chest"
+            >
+              <path d="M8 19a2 2 0 0 0 2-2V9a4 4 0 0 0-8 0v8a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a4 4 0 0 0-4-4H6" />
+              <path d="M2 11h20" />
+              <path d="M16 11v3" />
+            </svg>
+            <span className="ml-2">Inventário</span>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuItem className="cursor-pointer">
           <form action={signOut} className="w-full">
-            <button className="w-full text-start" type="submit">
+            <button
+              className="flex w-full items-center gap-2 text-start"
+              type="submit"
+            >
+              <LogOut size={18} />
               Sair
             </button>
           </form>
