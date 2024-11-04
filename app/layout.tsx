4c116659ac47373c241af1aa3b20513752b77b8f
João Pedro <jpmrc49@gmail.com>
2024-11-04 16:00:49 -0300

    fix: removes defaultUrl

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,7 @@ import { Toaster } from '@/components/ui/sonner'
 import { ReactQueryClientProvider } from '@/components/react-query-client-provider'
 import { createClient } from '@/utils/supabase/server'
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : 'http://localhost:3000'
-
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
   title: 'Cosmicus',
   description: '',
   icons: {
